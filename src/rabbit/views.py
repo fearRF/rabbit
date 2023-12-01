ec3bc8c9ee5f82e8e8d707778e9eb0c3c7fc4ca8
FearRF <lexumorganizm@gmail.com>
2023-12-01 13:55:36 +0300

    templates + auth

--- src/rabbit/views.py
+++ src/rabbit/views.py
@@ -1,2 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from . import forms
 
+
+def home(request):
+    return render(request, "rabbit/home.html")
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/home")
+    else:
+        form = forms.RegisterForm()
+
+    return render(request, "registration/sign-up.html", {"form": form})
--- src/src/settings.py
+++ src/src/settings.py
@@ -25,7 +25,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "rabbit",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -113,3 +120,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/home"
+
+LOGOUT_REDIRECT_URL = "/login"
